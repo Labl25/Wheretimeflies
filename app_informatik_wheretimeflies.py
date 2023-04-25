8490715c32f6f47f2233f0e6c057759630f24b2b
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-04-25 20:22:49 +0200

    Add files via upload

---
+++ app_informatik_wheretimeflies.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 18:32:14 2023
+
+@author: Startklar
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+import time 
+import plotly.express as px
+import altair as alt
+import json
+
+st.set_page_config(
+                    page_title="App_Informatik",
+                    page_icon="running",
+                    layout="wide",
+                    initial_sidebar_state="expanded"
+                    )
+
+
+st.title('Where time flies')
+
+#Inputs explanation
+st.subheader('Input categories')
+st.markdown('**Input explanation**')
+st.markdown('_Sleep hours_: Time spent actually sleeping.')
+st.markdown('_Time spent eating_: Time spent actually eating regardless of this beeing done while sitting or standing.')
+st.markdown('_Time spent sitting_: Time spent sitting, regardless of it being done while working or doing a hobby.')
+st.markdown('_Time spent walking_: Time spent only walking (from and to work, with your dog or just because).')
+st.markdown('_Time spent working out_: Time spent running, playing a sport or doing a workout (warm ups must be included).')
+st.markdown('_Time spent on hobby_: Time spent doing a non high active free-time activity like cooking, reading, scrolling through social media and such.')
+st.markdown('Every category can be left blank. However, the total of entered hours cannot exceed 24h.')
+st.markdown('_Remaining time_ is here a as guide to not threspass the 24h per day limit (_entered hours_ <= 24h).')
+
+# First step to store inputs of dataframe
+if "mdf" not in st.session_state:
+    st.session_state.mdf = pd.DataFrame(columns=
+                                       [
+                                            'Day and month', 
+                                            'Sleep hours', 
+                                            'Time spent eating', 
+                                            'Time spent sitting', 
+                                            'Time spent walking', 
+                                            'Time spent working out',
+                                            'Time spent on hobby'
+                                         ]
+                                        )
+
+# Create input columns
+Col0, Col1, Col2, Col3, Col4, Col5, Col6, Col7 = st.columns(8) 
+date = Col0.date_input(label = "Day and month")
+sleep = Col1.number_input(label="Sleep hours", min_value = 0.0, max_value=24.0) 
+food = Col2.number_input(label="Time spent eating", min_value = 0.0, max_value=24.0)
+sitting = Col3.number_input(label="Time spent sitting", min_value = 0.0, max_value=24.0) 
+walking = Col4.number_input(label="Time spent walking", min_value = 0.0, max_value=24.0) 
+workout = Col5.number_input(label="Time spent working out", min_value = 0.0, max_value=24.0)
+hobby = Col6.number_input(label="Time spent on hobby", min_value = 0.0, max_value=24.0)
+   
+# Calculate total_hours and not_accounted_for
+total_hours = sleep + food + sitting + walking + workout + hobby
+not_accounted_for = 24 - (total_hours) 
+# Define active as the sum of walking and workout hours
+active = walking + workout 
+
+# Create Col7 with not_accounted_for
+with Col7:
+    st.text('Remaining time') 
+    Col7.write(not_accounted_for)
+    
+# Being active recomendation, which if bieing less than 21 min, a warning will be present.
+if active < 0.35: 
+    st.warning("WHO recomends an adult person to be active for at least for 21 min per day") 
+    
+# Arrange 2 buttons into 2 columns
+Col0, Col1= st.columns(2) 
+# Show and submit button defined as run and show all data as run1      
+run_today = Col0.button("Show and submit today's data") 
+run_saved = Col1.button('Show all data', key = 'all_data')
+
+#Subheader of dataframe table
+st.subheader('Input data table')
+
+# Dataframe
+df = pd.DataFrame({'Day and month': date, 
+                   'Sleep hours': sleep,
+                   'Time spent eating': food,
+                   'Time spent sitting': sitting,
+                   'Time spent walking': walking,
+                   'Time spent working out': workout,
+                   'Time spent on hobby': hobby}, 
+                   index = [date]
+                   )             
+
+# Df2 as table from df1, which adds every new user input to the datatable
+df1 = pd.concat([st.session_state.mdf, df], ignore_index= True) 
+
+# Stop date duplications. Only the last date input will be shown in dataframe
+df2 = df1.drop_duplicates(subset=['Day and month'], keep='last')  
+ 
+# Redefine df1 and df2 ad df3    
+df3 = pd.DataFrame(df2) 
+SD = ('saved_data.csv')
+# read in existing and saved data
+df4 = pd.read_csv(SD)
+
+if run_today:
+    # Only user inputs with a total of max 24h will be added to the dataframe, otherwise warning will pop up.  
+    if total_hours <= 24:
+        # Check if df4 is empty or not
+        if df4.empty:
+            # If it's empty, assign the new input data to it
+            df4 = df3.copy()
+        else:
+            # If it's not empty, append the new input data to it while dropping duplicates
+            df4 = pd.concat([df4, df3], ignore_index=True)
+            df4 = df4.drop_duplicates(subset= ['Day and month'], keep='last')
+        
+        # Save dataframe as csv
+        df4.to_csv(SD, index=False)
+        
+        # Show dataframe df = User input
+        st.dataframe(df)
+        
+        # Descriptive title and text for chart from user input dataframe
+        st.subheader('Pie chart of today')
+        st.text('Percentage of time spent in each category today')
+        
+        # Create a piechart with df as dataframe
+        aggregated_data = df.drop('Day and month', axis=1).sum()
+        fig = px.pie(aggregated_data, values=aggregated_data.values, names=aggregated_data.index)
+        st.plotly_chart(fig)
+        
+    else:
+        st.warning("A day does not have more than 24 hours!") 
+ 
+if run_saved:
+    # Show df4 dataframe
+    st.dataframe(df4)
+    
+    # Descriptive title and text for chart from user input dataframe
+    st.subheader('Yearly graphical display')
+    st.text('Time spent for each category with latest input')
+    
+    # Create a barchart with older saved data (df4)
+    chart_data = pd.DataFrame(df4)
+    st.bar_chart(chart_data, x ='Day and month', 
+                 y = ['Sleep hours',
+                     'Time spent eating',
+                     'Time spent sitting',
+                     'Time spent walking',
+                     'Time spent working out',
+                     'Time spent on hobby']
+                 )
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
