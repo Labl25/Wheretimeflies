e8c7fec41579de06c0f5db6b9f43158259baa990
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-04-26 13:32:15 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -158,7 +158,7 @@ if run_saved:
     # Create a barchart with older saved data (df4)
     chart_data = pd.DataFrame(df4)
     st.bar_chart(chart_data, 
-                 x ='Day and month', 
+                 x =str('Day and month'), 
                  y = ['Sleep hours',
                      'Time spent eating',
                      'Time spent sitting',
