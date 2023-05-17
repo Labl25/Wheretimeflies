545bc354bb05aef01def21915341130e2436d010
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-17 11:34:13 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -139,11 +139,11 @@ df4 = load_key(api_key, bin_id, username)
 if run_today:
     # Only user inputs with a total of max 24h will be added to the dataframe; otherwise, a warning will pop up.
     if total_hours <= 24:
-        print(type(df4))
+        #print(type(df4))
         # Check if df4 is empty or not
-        if df4.empty:
+        #if df4.empty:
             # If it's empty, assign the new input data to it
-            df4 = df3.copy()
+            #df4 = df3.copy()
        # else:
             # If it's not empty, append the new input data to it while dropping duplicates
             df4 = pd.concat([df4, df3], ignore_index=True)
