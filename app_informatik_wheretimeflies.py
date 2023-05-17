c95db82368e3c0d77fc14d406a8ca8855100ec65
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-17 15:24:03 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -142,7 +142,7 @@ df2 = df1.drop_duplicates(subset=['Day and month'], keep='last')
 df3 = pd.DataFrame(df2) 
 # read in existing and saved data
 df4 = pd.read_json(DATA_FILE)
-st.write(session_state)
+st.write(st.session_state)
 if run_today:
     # Only user inputs with a total of max 24h will be added to the dataframe, otherwise warning will pop up.  
     if total_hours <= 24:
