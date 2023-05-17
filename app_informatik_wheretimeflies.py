a92be3376086bb48dd968ec70dda5c4292c59f67
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-17 13:42:54 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -128,7 +128,7 @@ df = {'Day and month': date,
 Col0, Col1= st.columns(2) 
 # Show and submit button defined as run and show all data as run1 
 with Col0:
-  st.button("Show and submit today's data"): 
+  st.button("Show and submit today's data"):
     # Only user inputs with a total of max 24h will be added to the dataframe; otherwise, a warning will pop up.
     if total_hours <= 24:
         df4 = load_key(api_key, bin_id, username)
