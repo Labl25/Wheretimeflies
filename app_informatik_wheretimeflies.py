04c8d6b4306de7ae8c52ff0c4b9579fb63506a4c
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-17 10:52:23 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -133,7 +133,8 @@ df2 = df1.drop_duplicates(subset=['Day and month'], keep='last')
 # Redefine df1 and df2 ad df3    
 df3 = pd.DataFrame(df2) 
 # read in existing and saved data
-df4 = pd.read_json(DATA_FILE)
+#df4 = pd.read_json(DATA_FILE)
+df4= load_key(api_key, bin_id, username)
 
 if run_today:
     # Only user inputs with a total of max 24h will be added to the dataframe, otherwise warning will pop up.  
@@ -148,7 +149,8 @@ if run_today:
             df4 = df4.drop_duplicates(subset= ['Day and month'], keep='last')
         
         # Save dataframe as json
-        df4.to_json('saved_data.json', orient='records')
+        #df4.to_json('saved_data.json', orient='records')
+        save_key(api_key, bin_id, key, df4)
         
         # Show dataframe df = User input
         st.dataframe(df)
