e9d08c3982fd513d758aef462600b05635eaab12
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-17 11:30:46 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -139,6 +139,7 @@ df4 = load_key(api_key, bin_id, username)
 if run_today:
     # Only user inputs with a total of max 24h will be added to the dataframe; otherwise, a warning will pop up.
     if total_hours <= 24:
+      print(type(df4))
         # Check if df4 is empty or not
         if df4.empty:
             # If it's empty, assign the new input data to it
