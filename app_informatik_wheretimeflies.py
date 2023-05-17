0a5604c98506782a7aeba4f18cd1c84fe27af26f
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-17 14:50:47 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -51,7 +51,59 @@ elif authentication_status == None:
     st.warning('Please enter your username and password')
     st.stop()
 
+st.title('Where time flies')
 
+#Inputs explanation
+st.subheader('Input categories')
+st.markdown('**Input explanation**')
+st.markdown('_Sleep hours_: Time spent actually sleeping.')
+st.markdown('_Time spent eating_: Time spent actually eating regardless of this beeing done while sitting or standing.')
+st.markdown('_Time spent sitting_: Time spent sitting, regardless of it being done while working or doing a hobby.')
+st.markdown('_Time spent walking_: Time spent only walking (from and to work, with your dog or just because).')
+st.markdown('_Time spent working out_: Time spent running, playing a sport or doing a workout (warm ups must be included).')
+st.markdown('_Time spent on hobby_: Time spent doing a non high active free-time activity like cooking, reading, scrolling through social media and such.')
+st.markdown('Every category can be left blank. However, the total of entered hours cannot exceed 24h.')
+st.markdown('_Remaining time_ is here a as guide to not threspass the 24h per day limit (_entered hours_ <= 24h).')
+
+# First step to store inputs of dataframe
+if "mdf" not in st.session_state:
+    st.session_state.mdf = pd.DataFrame(columns=
+                                       [
+                                            'Day and month', 
+                                            'Sleep hours', 
+                                            'Time spent eating', 
+                                            'Time spent sitting', 
+                                            'Time spent walking', 
+                                            'Time spent working out',
+                                            'Time spent on hobby'
+                                         ]
+                                        )
+
+# Create input columns
+Col0, Col1, Col2, Col3, Col4, Col5, Col6, Col7 = st.columns(8) 
+date = Col0.date_input(label = "Day and month").strftime("%Y-%m-%d")
+sleep = Col1.number_input(label="Sleep hours", min_value = 0.0, max_value=24.0) 
+food = Col2.number_input(label="Time spent eating", min_value = 0.0, max_value=24.0)
+sitting = Col3.number_input(label="Time spent sitting", min_value = 0.0, max_value=24.0) 
+walking = Col4.number_input(label="Time spent walking", min_value = 0.0, max_value=24.0) 
+workout = Col5.number_input(label="Time spent working out", min_value = 0.0, max_value=24.0)
+hobby = Col6.number_input(label="Time spent on hobby", min_value = 0.0, max_value=24.0)
+   
+# Calculate total_hours and not_accounted_for
+total_hours = sleep + food + sitting + walking + workout + hobby
+not_accounted_for = 24 - (total_hours) 
+# Define active as the sum of walking and workout hours
+active = walking + workout 
+
+# Create Col7 with not_accounted_for
+with Col7:
+    st.text('Remaining time') 
+    Col7.write(not_accounted_for)
+    
+# Being active recomendation, which if bieing less than 21 min, a warning will be present.
+if active < 0.35: 
+    st.warning("WHO recomends an adult person to be active for at least for 21 min per day") 
+    
 # Arrange 2 buttons into 2 columns
 Col0, Col1= st.columns(2) 
 # Show and submit button defined as run and show all data as run1      
@@ -131,7 +183,6 @@ if run_saved:
                      'Time spent working out',
                      'Time spent on hobby']
                  )
-
 
  
     
