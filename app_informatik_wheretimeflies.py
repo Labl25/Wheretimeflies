99e52f957d88a9a6f5405f2111c9fbbfba727ae1
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-17 13:47:02 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -141,6 +141,8 @@ with Col0:
         ##df4 = pd.concat([df4, df3], ignore_index=True)
         ##df4 = df4.drop_duplicates(subset= ['Day and month'], keep='last')
         st.write(df4)
+        df=pd.DataFrame(df)
+        df4=pd.DataFrame(df4)
         df4=df.append(df4)
         df4=dict(df4)
         save_key(api_key, bin_id, username, df4)
