eb55702b9e7c803da3ef4d348c439cb107b781f3
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-17 13:17:57 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -129,7 +129,6 @@ df1=pd.DataFrame(df,index = [date])
 # read in existing and saved data
 #df4 = pd.read_json(DATA_FILE)
 df4 = load_key(api_key, bin_id, username)
-df4=pd.DataFrame(df4,index = [date])
 #df4 =pd.DataFrame(df4)
 
 
@@ -144,7 +143,7 @@ if run_today:
             # If it's not empty, append the new input data to it while dropping duplicates
         ##df4 = pd.concat([df4, df3], ignore_index=True)
         ##df4 = df4.drop_duplicates(subset= ['Day and month'], keep='last')
-        df4=df4.append(df1)
+        df4=df4.append(df)
         save_key(api_key, bin_id, username, df4)
         df4 = load_key(api_key, bin_id, username)
         # Show dataframe df = User input
