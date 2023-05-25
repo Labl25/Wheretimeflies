de5f72499ee0dd1edf790db32a1d3484f067aec3
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-25 15:55:52 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -163,10 +163,9 @@ if run_saved:
  if delete:
     accu_data = load_key(api_key, bin_id, username)
     df1 = pd.DataFrame(accu_data)
-     if not df1.empty:
+    if not df1.empty:
         st.dataframe(df1)
         if st.button("Delete Last Row"):
-            
             df1 = df1.drop(df1.index[-1])
             accu_data = df1.to_dict(orient='records')
             res = save_key(api_key, bin_id, username, accu_data)
