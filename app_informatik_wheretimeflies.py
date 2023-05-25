742a005fc6270e5854333f46850aa818f9765f8b
Labl25 <131799814+Labl25@users.noreply.github.com>
2023-05-25 13:31:54 +0200

    Update app_informatik_wheretimeflies.py

--- app_informatik_wheretimeflies.py
+++ app_informatik_wheretimeflies.py
@@ -131,7 +131,7 @@ df2 = df1.drop_duplicates(subset=['Day and month'], keep='last')
 df3 = pd.DataFrame(df2) 
 # read in existing and saved data
 #df4 = pd.read_json(DATA_FILE)
-df4 = load_data(api_key, bin_id)
+#df4 = load_data(api_key, bin_id)
 #df4 = load_key(api_key, bin_id, username)
 
 #st.write(df4)
